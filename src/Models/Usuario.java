d43a64206004b0112cc1593fb99970183da64469
Leonardo <leonarddo.gomees@gmail.com>
2023-03-04 11:25:51 -0300

    Criando aplicação de agendamento para uma barbearia

---
+++ src/Models/Usuario.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+import java.util.Date;
+
+/**
+ *
+ * @author leona
+ */
+public class Usuario extends Pessoa {
+    
+   
+    private String senha;
+    private String nivelAcesso;
+
+    public Usuario(int id, String nome, char sexo, String dataNascimento, String telefone, String email, String cpf, String senha, String nivelAcesso) {
+        super(id, nome, sexo, dataNascimento, telefone, email, cpf);
+        this.senha = senha;
+        this.nivelAcesso = nivelAcesso;
+    }
+
+    public Usuario(int id, String nome, String senha, String nivelAcesso) {
+        super(id, nome);
+        this.senha = senha;
+        this.nivelAcesso = nivelAcesso;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNivelAcesso() {
+        return nivelAcesso;
+    }
+
+    public void setNivelAcesso(String nivelAcesso) {
+        this.nivelAcesso = nivelAcesso;
+    }
+    
+    
+    
+
+ 
+    
+}
+    
+
+   
+
+    
---
+++ src/View/Main.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+
+import Models.Agendamento;
+import Models.Cliente;
+import Models.Servico;
+import Models.Usuario;
+
+/**
+ *
+ * @author leona
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        Servico servico = new Servico(1,"Barba",20); 
+        
+        System.out.println(servico.getDescricao());
+        System.out.println(servico.getValor());
+        
+        
+        Cliente cliente = new Cliente (1, "Leonardo", "Rua teste","1234545");
+        
+        System.out.println(cliente.getNome());
+        
+        
+        Usuario usuario = new Usuario(2, "Barbeiro", "1234", "2");
+        
+                System.out.println(usuario.getNome());
+                
+                
+                
+        Agendamento agendamento = new Agendamento (1, cliente, servico, "06/03/2023 11:17");        
+        
+        
+        System.out.println(agendamento.getCliente().getNome());
+
+        
+    }
+    
+}
